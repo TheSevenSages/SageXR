48f211560d0579e10af16d8b906fd113e68b072b
Jacob Robinson <Jacobrobinson4321@gmail.com>
2025-11-06 15:47:01 -0500

    Add Whisper Package for STT

---
+++ UnityVRTest/Assets/Scripts/AI/Chatbot.cs
@@ -0,0 +1,40 @@
+using LLMUnity;
+using System;
+using UnityEngine;
+
+public class Chatbot : MonoBehaviour
+{
+    // Is called periodically while the response is being recieved
+    void HandleReply(string reply)
+    {
+        //Debug.Log($"AI RESPONSE: {reply}");
+    }
+
+    // Is called when the AI has finished formulating its response
+    void ReplyCompleted()
+    {
+        Debug.Log("AI IS DONE RESPONDING");
+    }
+
+    public async void SendAIMessage(string message)
+    {
+        try
+        {
+            LLMCharacter character = gameObject.GetComponent<LLMCharacter>();
+            if (character == null)
+            {
+                throw new Exception("LLMCharacter component is missing from this GameObject");
+            }
+            else
+            {
+                character.CancelRequests(); // Cancel any requests to this character that are in progress
+                var response = await character.Chat(message, HandleReply, ReplyCompleted);
+                Debug.Log($"AI RESPONSE: {response}");
+            }
+        }
+        catch (Exception e)
+        {
+            Debug.LogException(e);
+        }
+    }
+}
