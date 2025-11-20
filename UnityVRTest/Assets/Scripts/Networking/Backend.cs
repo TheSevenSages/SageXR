31150853f29f642e3d04dca1a9b5788f95de2f8d
Jacob Robinson <Jacobrobinson4321@gmail.com>
2025-11-20 17:58:56 -0500

    Device Init
    Backend.cs registers the device [that the software is running on] with the server automatically.

--- UnityVRTest/Assets/Scripts/Networking/Backend.cs
+++ UnityVRTest/Assets/Scripts/Networking/Backend.cs
@@ -3,13 +3,18 @@ using Microsoft.AspNetCore.SignalR.Client;
 
 public class Backend : MonoBehaviour
 {
+    [Header("Settings")]
+    public string deviceName = "DEVICE_NAME";
+    public bool isHost = false;
+
+    [Header("API Location")]
     [SerializeField]
     string ip = "127.0.0.1";
     [SerializeField]
     string port = "8080";
 
     private HubConnection connection;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+   
     async void Start()
     {
         // Build the connection
@@ -18,11 +23,11 @@ public class Backend : MonoBehaviour
             .Build();
 
         await connection.StartAsync();
+        await connection.SendAsync("InitDevice", deviceName, isHost);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void GetMessage(string message)
     {
-        
+        Debug.Log(message);
     }
 }
